2e81efcc50b0b561d1dffb220907f9c8a9904434
edem8 <eddieblack159@gmail.com>
2023-09-07 23:38:38 +0000

    chat implementation using socketio

--- app.py
+++ app.py
@@ -1,36 +1,47 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, session
-from flask_socketio import SocketIO
 import paho.mqtt.client as mqtt
 import json
+from flask_socketio import SocketIO, send
 
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = "secret!123"
+socketio = SocketIO(app, cors_allowed_origin="*", async_mode='threading')
 
 # MQTT client setup
 mqtt_client = mqtt.Client()
 
 # Define MQTT connection parameters
 MQTT_BROKER_HOST = "mqtt.eclipseprojects.io"
+
 MQTT_BROKER_PORT = 1883
-MQTT_CHAT_TOPIC = ""
+MQTT_CHAT_TOPIC = "glblcd"
+
 
 # Function to set up MQTT
 def setup_mqtt():
     global mqtt_client
     if not mqtt_client.is_connected():
-        mqtt_client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT)
+        mqtt_client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT, 60)
+
         mqtt_client.loop_start()
 
 
 # MQTT callback functions
 def on_connect(client, userdata, flags, rc):
     print(f"Connected to MQTT broker with result code {rc}")
+    client.subscribe(MQTT_CHAT_TOPIC)
     # Subscribe to the chat topic upon successful connection
 
 
 def on_message(client, userdata, msg):
     print(f"Received message on topic {msg.topic}: {msg.payload.decode()}")
+    message = msg.payload.decode()
+    
+
+  
+    
+    
 
 
 # Set MQTT callback functions
@@ -50,11 +61,12 @@ def signup():
     return render_template("signup.html")
 
 
-"""
+# Define a global variable for MQTT_CHAT_TOPIC
 @app.route("/search", methods=["POST"])
 def search():
+    global query
     query = request.form.get("query")
-
+    print("Query: ", query)
     with open("./static/json/groups.json") as json_file:
         groups = json.load(json_file)
         results = [
@@ -62,48 +74,15 @@ def search():
             for group in groups
             if query == group["groupName"].lower()
         ]
-    try:
-        global MQTT_CHAT_TOPIC
-        MQTT_CHAT_TOPIC = results[0]
-    except IndexError:
-        return render_template("chat.html")
-    print(results)
+    if results:
+        result = results[0]
+        print("Finally", result)
+        setup_mqtt()
+
+    else:
+        print("No result")
 
     return render_template("chat.html", results=results)
-
-    """
-
-
-# Define a global variable for MQTT_CHAT_TOPIC
-@app.route("/search", methods=["POST", "GET"])
-def search():
-    query = request.form.get("query")
-
-    with open("./static/json/groups.json") as json_file:
-        groups = json.load(json_file)
-        results = [
-            group["groupName"]
-            for group in groups
-            if query == group["groupName"].lower()
-        ]
-
-    
-    try:
-        MQTT_CHAT_TOPIC = results[0]
-        print(MQTT_CHAT_TOPIC)
-        setup_mqtt()
-        mqtt_client.subscribe(MQTT_CHAT_TOPIC)
-
-    except IndexError:
-        MQTT_CHAT_TOPIC = ""  # Set to an empty string if no results are found
-
-    # Create a response dictionary to send JSON data
-
-    response_data = {
-        "results": results,
-    }
-
-    return jsonify(response_data)
 
 
 # Function to validate user logins
@@ -158,12 +137,25 @@ def register():
 def update_content():
     message = request.form.get("message")
     if message:
-        
+        # setup_mqtt()
         mqtt_client.publish(MQTT_CHAT_TOPIC, message)
-        print(message)
-    response_data = {"new_content": message}
-    return jsonify(response_data)
+       
 
+    return "", 204
+
+
+# diconnect a client
+@app.route("/disconnect")
+def disconnect():
+    mqtt_client.disconnect()
+    print("disconnect")
+
+    return redirect("/")
+
+@socketio.on("message")
+def handle_message(message):
+    if message != "User connected!":
+        send(message)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    socketio.run(app, host="localhost")
